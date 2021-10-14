f5c64017a13f4cd80411d0b90772180641a4d900
Mitch-coder <michellescalderon2002@gmail.com>
2021-10-14 00:18:27 -0500

    funcionesFlechas

--- Basic/code.js
+++ Basic/code.js
@@ -37,7 +37,30 @@
             //%= operacion del resto numero1&numero2
             //**=  esto es exponencializacion que esta rarisima    
             /*La sintaxis de los condicionales es la misma que en los otros lenguajes*/
-            
+        /*Loops*/
+            //El bucle while es igual
+            //--->Existen diferentes fors el de sintaxis basica, 
+            //uno de decremento y aumento si asi se requiere
+            for (i = 0, j = 5; i < 5; i++, j--) {
+                console.log("Valor de i y j:", i, j);
+            }
+            //La instruccion continue sirve para saltarsr la iteracion de un bucle
+            //Arreglo de ejemplo
+            animals =["Gato","Perro","Chocoyo"]
+            //El for in te devuelve posicion
+            forMichelle:
+            for (const animal in animals) {
+                document.write(animal);
+                break forMichelle;
+            }
+            //El for of te devuelve valor
+            for (const animal of animals) {
+                document.write(animal);
+            }
+            //Podemos llamar a los bloques de fors con label y despues quebrarlos
+        /*Funciones Flechas*/
+            const grettings = (names) =>document.write(`Hola Como estas?${names}`);
+            grettings(prompt("Cual es tu nombre"));
         /*Nota: en JavaScript los metodos por default tambien vienen en camel*/
         
 
